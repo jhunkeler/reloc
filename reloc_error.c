062ae1ba5f5661b249366969e03024992f843b5d
Joseph Hunkeler <jhunkeler@gmail.com>
2019-11-14 09:26:42 -0500

    Add initial error handler code

---
+++ reloc_error.c
@@ -0,0 +1,21 @@
+#include "reloc.h"
+
+int reloc_error = 0;
+const char *_reloc_errors[] = {
+    "Success",
+    "Read error",
+    "Write error",
+    "Verification failure",
+    "Length too long",
+    "Memory allocation failure",
+    NULL,
+};
+
+
+const char *reloc_strerror(int code) {
+    return _reloc_errors[code];
+}
+
+void reloc_perror(const char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, reloc_strerror(reloc_error));
+}
